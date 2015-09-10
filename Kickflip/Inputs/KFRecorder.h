64bb548808e20dc21731aebf2e622cf4713ec4be
Jassadakorn Ketkaew <wizard-world@hotmail.co.th>
2015-09-10 18:02:17 +0700

    update

--- Kickflip/Inputs/KFRecorder.h
+++ Kickflip/Inputs/KFRecorder.h
@@ -19,6 +19,7 @@
 - (void) recorderDidStartRecording:(KFRecorder*)recorder error:(NSError*)error;
 - (void) recorderDidFinishRecording:(KFRecorder*)recorder error:(NSError*)error;
 - (void) recorder:(KFRecorder*)recorder streamReadyAtURL:(NSURL*)url;
+- (void) updateLocation:(KFRecorder *)recorder;
 @end
 
 /**
@@ -50,6 +51,8 @@
 @property (nonatomic, weak) id<KFRecorderDelegate> delegate;
 
 - (void) startRecording;
+- (void) startRecordingWithParam:(NSDictionary*)parameters;
 - (void) stopRecording;
+- (void) switchCameraWithButton:(UIButton *)btn;
 
 @end
